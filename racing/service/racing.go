bd21c22d5efff7df554441e8ef1be2ecc4551bda
Ramesh <rameshs24@gmail.com>
2023-08-07 12:59:41 +1000

    #4: Introduce a new RPC, that allows us to fetch a single race by its ID

--- racing/service/racing.go
+++ racing/service/racing.go
@@ -9,6 +9,9 @@ import (
 type Racing interface {
 	// ListRaces will return a collection of races.
 	ListRaces(ctx context.Context, in *racing.ListRacesRequest) (*racing.ListRacesResponse, error)
+
+	// Get Single Race by ID
+	GetRace(ctx context.Context, filter *racing.GetRaceRequest) (*racing.GetRaceResponse, error)
 }
 
 // racingService implements the Racing interface.
@@ -28,4 +31,12 @@ func (s *racingService) ListRaces(ctx context.Context, in *racing.ListRacesReque
 	}
 
 	return &racing.ListRacesResponse{Races: races}, nil
+}
+
+func (s *racingService) GetRace(ctx context.Context, filter *racing.GetRaceRequest) (*racing.GetRaceResponse, error) {
+	race, err := s.racesRepo.GetRace(filter)
+	if err != nil {
+		return nil, err
+	}
+	return &racing.GetRaceResponse{Race: race}, nil
 }
